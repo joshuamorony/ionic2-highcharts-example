463059f9115c90e6f9fdba236898a4f8e274ef35
Joshua Morony <joshuamorony@Joshuas-MacBook-Pro.local>
2017-03-26 09:05:37 +1030

    initial commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,60 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import Highcharts from 'highcharts'
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+	constructor(public navCtrl: NavController) {
+
+	}
+
+	ionViewDidLoad(){
+
+		Highcharts.chart('container', {
+
+		    title: {
+		        text: 'Solar Employment Growth by Sector, 2010-2016'
+		    },
+
+		    subtitle: {
+		        text: 'Source: thesolarfoundation.com'
+		    },
+
+		    yAxis: {
+		        title: {
+		            text: 'Number of Employees'
+		        }
+		    },
+
+		    plotOptions: {
+		        series: {
+		            pointStart: 2010
+		        }
+		    },
+
+		    series: [{
+		        name: 'Installation',
+		        data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
+		    }, {
+		        name: 'Manufacturing',
+		        data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
+		    }, {
+		        name: 'Sales & Distribution',
+		        data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
+		    }, {
+		        name: 'Project Development',
+		        data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
+		    }, {
+		        name: 'Other',
+		        data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
+		    }]
+
+		});
+
+	}
+
+}
